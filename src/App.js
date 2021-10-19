27082fa91864f64f35c3ef4bc67ec4804944d1fd
jamiemn86 <jamie.a.mollernielsen@gmail.com>
2021-10-19 16:57:14 +0200

    Basics of iteration 3 working

--- src/App.js
+++ src/App.js
@@ -23,14 +23,17 @@ class App extends React.Component {
   handleChange = (event) => {
     console.log('handleChange was executed');
     console.log(event.target.value);
+    console.log(this.state.newFoodAddition);
     const name = event.target.name;
     const type = event.target.type;
     let value =
       type === 'number' ? event.target.valueAsNumber : event.target.value;
     // if (Number.isNaN(value)) value = 0;
     if (Number.isNaN(value)) value = '';
-    this.setState({
-      [name]: value,
+    this.setState((previousState) => {
+      return {
+        newFoodAddition: { ...previousState.newFoodAddition, [name]: value },
+      };
     });
   };
   addFoodItem = (event) => {
